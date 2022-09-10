33d6c668f0164af55647c336a17f1da119b7b367
Sachi Goto <70562492+SachiGoto@users.noreply.github.com>
2022-09-09 23:28:48 -0700

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
-import { GreenHomeComponent } from './greenaroundtheworld/home/home.component';
-import { GreenProductsComponent } from './greenaroundtheworld/products/products.component';
-import {GreenProductDetailComponent} from './greenaroundtheworld/product-detail/product-detail.component'
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ProjectDetailsComponent } from './project-details/project-details.component';
-
 import { ProjectsComponent } from './projects/projects.component';
+
+import { GreenHomeComponent } from './greenaroundtheworld/home/home.component';
+import { GreenProductsComponent } from './greenaroundtheworld/products/products.component';
+import {GreenProductDetailComponent} from './greenaroundtheworld/product-detail/product-detail.component'
 import { GreenAboutComponent } from './greenaroundtheworld/about/about.component';
 import { GreenFooterComponent } from './greenaroundtheworld/footer/footer.component';
 
@@ -28,8 +28,8 @@ const routes: Routes = [
    {path:"contact", component:ContactComponent},
    {path:"navbar", component:NavbarComponent},
 
-   {path:"ecommerce", component:HomeComponent},
-   {path:"green-home", component:GreenHomeComponent},
+  //  {path:"ecommerce", component:HomeComponent},
+   {path:"greenaroundtheworld/home", component:GreenHomeComponent},
    {path:"green-products", component:GreenProductsComponent},
    {path:"green-products/:id", component:GreenProductDetailComponent},
    {path:"green-about", component:GreenAboutComponent},
